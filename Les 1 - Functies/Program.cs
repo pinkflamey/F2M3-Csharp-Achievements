669a6b631cefd12d1351e74c143d4bb50776ee65
pinkflamey <mavis@pinkflamey.nl>
2022-03-04 12:24:47 +0100

    les 1 - functies afgerond

---
+++ Les 1 - Functies/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Les_1___Functies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string AddTwoWords(string word1, string word2)
+            {
+                return word1 + " " + word2;
+            }
+
+            Console.WriteLine("Enter word 1: ");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("Enter word 2: ");
+            string word2 = Console.ReadLine();
+
+            Console.WriteLine("Result: " + AddTwoWords(word1, word2));
+
+        }
+    }
+}
