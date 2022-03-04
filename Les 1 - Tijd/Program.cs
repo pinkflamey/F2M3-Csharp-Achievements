e95133762d903baaabf9ae76b1dda7098f1b347f
pinkflamey <mavis@pinkflamey.nl>
2022-03-04 12:09:19 +0100

    les 1 afgerond

--- Les 1 - Tijd/Program.cs
+++ Les 1 - Tijd/Program.cs
@@ -6,7 +6,18 @@ namespace Les_1___Tijd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            DateTime dateTime = DateTime.Now;
+
+            Console.WriteLine(dateTime.ToString("dddd, dd MMMM yyyy"));
+            Console.WriteLine(dateTime.ToString("T"));
+
+            Console.WriteLine("\nDe maand is " + dateTime.Month + ".");
+            Console.WriteLine("Het is dag " + dateTime.Day + ".");
+            Console.WriteLine("Het uur is " + dateTime.Hour + ".");
+            Console.WriteLine("Dit uur zijn er " + dateTime.Minute + " minuten geweest.");
+            Console.WriteLine("Deze minuut zijn er " + dateTime.Second + " seconden geweest.");
+
         }
     }
 }
