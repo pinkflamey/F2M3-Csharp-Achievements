3d19774626ed3dc40e17085b61913ca3f76e9445
pinkflamey <mavis@pinkflamey.nl>
2022-03-04 12:44:38 +0100

    les 1 functions return afgerond

---
+++ Les 1 - Functions return/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Les_1___Functions_return
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string myName(string name)
+            {
+                return "\nYour name is " + name + "!";
+            }
+
+            Console.WriteLine("Enter your name:");
+            string enteredName = Console.ReadLine();
+            Console.WriteLine(myName(enteredName));
+            
+            Thread.Sleep(1500);
+
+        }
+    }
+}
