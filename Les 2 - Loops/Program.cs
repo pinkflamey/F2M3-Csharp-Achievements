0123c487bdf9aad6591f0ff5186bfaa91d53e65b
pinkflamey <mavis@pinkflamey.nl>
2022-03-07 10:46:05 +0100

    les 2 deel 3

---
+++ Les 2 - Loops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Les_2___Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { 0, 2, 4, 6, 8, 10 };
+
+            Console.WriteLine("Manier 1:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine("\nManier 2:");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
