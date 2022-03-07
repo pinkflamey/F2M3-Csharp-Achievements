83a7d7fe7fc2c0b9db7565807ecf489ba9f71d5c
pinkflamey <mavis@pinkflamey.nl>
2022-03-07 10:38:39 +0100

    les 2 deel 2 afgerond

---
+++ Les 2 - Arrays/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Les_2___Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers1 = { 10, 20, 30, 40, 50, 60 };
+
+            Console.WriteLine("-----");
+            Console.WriteLine("Starting array:");
+
+            foreach (int i in numbers1)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("-----");
+
+            int arrayLength = numbers1.Length;
+            Console.WriteLine("Array length is " + arrayLength);
+            Console.WriteLine("-----");
+
+            int[] numbers2 = new int[6];
+            int counter = 0;
+
+            for (int i = arrayLength - 1; i >= 0; i--)
+            {
+
+                numbers2[counter] = numbers1[i];
+                counter++;
+
+                Console.WriteLine("Given array 2 section " + counter + " the content of array 1 section " + i + ": " + numbers1[i]);
+            }
+
+            Console.WriteLine("-----");
+            Console.WriteLine("Array backwards:");
+            
+            foreach (int i in numbers2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("-----");
+
+
+        }
+    }
+}
