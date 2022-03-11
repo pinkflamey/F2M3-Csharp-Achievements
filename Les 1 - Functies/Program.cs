7db118da10274bf24e89a67fc65e2e78e97d40d9
pinkflamey <mavis@pinkflamey.nl>
2022-03-11 13:16:25 +0100

    to gold

--- Les 1 - Functies/Program.cs
+++ Les 1 - Functies/Program.cs
@@ -4,19 +4,28 @@ namespace Les_1___Functies
 {
     internal class Program
     {
+
+        public string AddTwoWords(string word1, string word2)
+        {
+            return word1 + " " + word2;
+        }
         static void Main(string[] args)
         {
-            string AddTwoWords(string word1, string word2)
-            {
-                return word1 + " " + word2;
-            }
+            Program pr = new Program();
 
             Console.WriteLine("Enter word 1: ");
             string word1 = Console.ReadLine();
             Console.WriteLine("Enter word 2: ");
             string word2 = Console.ReadLine();
 
-            Console.WriteLine("Result: " + AddTwoWords(word1, word2));
+            string result = pr.AddTwoWords(word1, word2);
+
+            Console.WriteLine("Result: " + result);
+
+            char[] array = result.ToCharArray();
+            Array.Reverse(array);
+
+            Console.WriteLine("Result backwards: " + new string(array));
 
         }
     }
--- Les 1 - Functions return/Program.cs
+++ Les 1 - Functions return/Program.cs
@@ -12,12 +12,24 @@ namespace Les_1___Functions_return
             {
                 return "\nYour name is " + name + "!";
             }
+            string randomNumber()
+            {
+                Random r = new Random();
+                int rInt = r.Next(1, 20);
+                return rInt.ToString();
+            }
 
             Console.WriteLine("Enter your name:");
             string enteredName = Console.ReadLine();
             Console.WriteLine(myName(enteredName));
             
-            Thread.Sleep(1500);
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Random number 1-20: " + randomNumber());
+            Console.WriteLine("Random number 1-20: " + randomNumber());
+            Console.WriteLine("Random number 1-20: " + randomNumber());
+
+            Thread.Sleep(1000);
 
         }
     }
