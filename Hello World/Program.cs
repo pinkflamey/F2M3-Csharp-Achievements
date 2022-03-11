94124dfab001c0fd745b254cbedd3e5828271ba0
pinkflamey <mavis@pinkflamey.nl>
2022-03-11 13:42:37 +0100

    to gold

---
+++ Hello World/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            string helloWorld = "Hello World!";
+
+            foreach (char c in helloWorld)
+            {
+                Console.Write(c);
+                Thread.Sleep(300);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
