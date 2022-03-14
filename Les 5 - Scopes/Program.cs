2e0a06da0c1e164cc9a7db8ef2fafeddf5c0ab73
pinkflamey <mavis@pinkflamey.nl>
2022-03-14 09:58:03 +0100

    les 5 gold

---
+++ Les 5 - Scopes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Les_5___Scopes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            Console.WriteLine(program.testFunction(true, true, 9));
+        }
+
+        internal bool testFunction(bool x, bool y, int length)
+        {
+            bool testBool = false;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (x && y)
+                {
+                    testBool = true;
+                    x = true;
+                }
+                else
+                {
+                    testBool = false;
+                    y = false;
+                }
+            }
+            return testBool;
+        }
+    }
+}
